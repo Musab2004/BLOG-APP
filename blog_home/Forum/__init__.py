91a82f90590ff07094fffe699a6696ff59f49eb8
Musab bin abaid <110807964+Musab2004@users.noreply.github.com>
2023-08-23 23:22:09 +0500

    Delete blog_home directory

--- blog_home/Forum/__init__.py
+++
@@ -1,1 +0,0 @@
-from .blog_Form import *
--- blog_home/context_processors.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth import get_user
-
-def navbar_parameters(request):
-    user = get_user(request)
-    return {
-        'session_user': user,
-    }
